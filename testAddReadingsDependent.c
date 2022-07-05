e666c1cabdb6a5340b6d8ef4bcdc214bf63d9659
Uriel <urielarias.a12@gmail.com>
2022-07-05 14:47:47 -0300

    Tests for addReadings dependent functions

--- testAddReadingsDependent.c
+++ testAddReadingsDependent.c
@@ -10,32 +10,27 @@
 
 
 /*
- * int addReading(peatonesADT pea, int year, char * month, int mDate, char * day, int sensorId, int time, int counts, const int FromTo[2]){
+ * int addReading(peatonesADT pea, int sensorId, const int date[DATE_FIELDS], const char * day, int counts, const int FromTo[2]){
+    if (!(sensorExists(pea, sensorId)))return 0;
+
     TSensor sensor = pea->sensorsVec[sensorId-1];
-    if (pea->sensorsSize < sensorId||sensor.id==-1) return 0;
     sensor.sensorCounts += counts;
     //si esta entre los anios del rango provisto y counts es mayor que el counts que habia en maxCounts, lo modifica
-    if (FromTo[0]==0 ||((year>=FromTo[0] && (year<=FromTo[1]))||FromTo[1]==0)){
+    if (FromTo[0]==0 ||((date[YEAR]>=FromTo[0] && (date[YEAR]<=FromTo[1]))||FromTo[1]==0)){
         if (sensor.maxCount.counts < counts){
-            sensor.maxCount.dateFormatted[DAY]=mDate;
-            sensor.maxCount.dateFormatted[MONTH]= monthToNum(month);
-            sensor.maxCount.dateFormatted[YEAR]=year;
-            sensor.maxCount.dateFormatted[HOUR]=time;
+            sensor.maxCount.counts = counts;
+            sensor.maxCount.dateFormatted[DAY]=date[DAY];
+            sensor.maxCount.dateFormatted[MONTH]= date[MONTH];
+            sensor.maxCount.dateFormatted[YEAR]=date[YEAR];
+            sensor.maxCount.dateFormatted[HOUR]=date[HOUR];
         }
     }
     pea->sensorsVec[sensorId-1]=sensor; //paso al ADT lo que modifique
 
-    toBeginYear(pea);
-    while (hasNextYear(pea) && pea->next->year < year){
-        pea->next = pea->next->tail;
-    }
-    if (pea->next->year > year){
-        pea->next = addYear(pea->next, year);
-    }
-    pea->next->yearCount += counts;
+    pea->first = addYear(pea->first, date[YEAR], counts);
 
     int weekDay = weekDayToNum(day);
-    if (time >= 6 && time < 18){
+    if (date[HOUR] >= 6 && date[HOUR] < 18){
         pea->dayVec[weekDay].daylightCount += counts;
     } else
         pea->dayVec[weekDay].nightCount += counts;
@@ -76,40 +71,70 @@ int getMaxReadingById(peatonesADT pea, int id, int * maxCount, char ** name, int
 int main(void) {
     peatonesADT tad = newPeatones();
     int range[2] = {2015, 2022};
+
+    int date1[DATE_FIELDS] = {30,5,2021,12};
+    int date2[DATE_FIELDS] = {21,2,2015,22};
+    int date3[DATE_FIELDS] = {21,0,2012,3};
+    int date4[DATE_FIELDS] = {13,2,2015,22};
+    int date5[DATE_FIELDS] = {31,11,2022,23};
+    //incorporamos los sensores que deberían aparecer en el archivo de sensors usando la función ya testeada
     putSensor(tad, 40, "Street 1");
     putSensor(tad, 140, "Street 34");
-    addReading(tad, 2021, "June", 30, "Wednesday", 40, 12, 2389389, range);
-    addReading(tad, 2015, "March", 21, "Thursday", 140, 22, 1761879, range);
-    addReading(tad, 2012, "January", 21, "Friday", 146, 3, 17618793, range);
-    addReading(tad, 2017, "March", 13, "Saturday", 140, 13, 7836829, range);
-    addReading(tad, 2022, "December", 31, "Sunday", 40, 23, 9289726, range);
+    //agregamos mediciones utiles para chequear funciones
+    addReading(tad, 40, date1, "Wednesday", 89389, range);
+    addReading(tad, 140, date2, "Thursday", 61879, range);
+    addReading(tad, 146, date3, "Friday", 1879, range);
+    addReading(tad, 140, date4, "Saturday", 36829, range);
+    addReading(tad, 40, date5, "Sunday", 89726, range);
     toBeginYear(tad);
     int year;
     long int yearCount;
+    //verificamos que los años queden ordenados en orden descendente
     nextYear(tad, &yearCount, &year);
     assert(year == 2022);
-    assert(yearCount == 9289726);
+    assert(yearCount == 89726);
     nextYear(tad, &yearCount,&year);
     assert(year == 2021);
-    assert(yearCount == 2389389);
+    assert(yearCount == 89389);
     nextYear(tad, &yearCount,&year);
     assert(year == 2017);
-    assert(yearCount == 7836829);
+    assert(yearCount == 36829);
     nextYear(tad, &yearCount,&year);
     assert(year == 2015);
-    assert(yearCount == 1761879);
-
-    assert(getDailyCount(tad, 2, 0) == 2389389);
+    assert(yearCount == 61879);
+    //verificamos que la medición no sea incorporada porque correspondería a un sensor no incluido en el archivo de sensores de importancia
+    assert(hasNextYear(tad) == 0);
+    //verificamos que se actualice el iterador
+    toBeginYear(tad);
+    nextYear(tad, &yearCount, &year);
+    assert(year == 2022);
+    assert(yearCount == 89726);
+    //verificamos por día de la semana que se hayan actualizado los contadores en función del horario de las mediciones
+    //daysType { MONDAY=0, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY, CANT_DAYS }
+    assert(getDailyCount(tad, 2, 0) == 89389);
     assert(getDailyCount(tad, 2, 1) == 0);
     assert(getDailyCount(tad, 3, 0) == 0);
-    assert(getDailyCount(tad, 3, 1) == 1761879);
-    assert(sensorExists(tad, 146) == 0);
+    assert(getDailyCount(tad, 3, 1) == 61879);
     assert(getDailyCount(tad, 4, 1) == 0);
-
+    //verifico que el sensor no esta
+    assert(sensorExists(tad, 146) == 0);
     int maxCount;
     char * sensorName;
     int date[DATE_FIELDS];
+    int j;
+    //verificamos que se guarde solo la medición máxima tomada por cada sensor
     getMaxReadingById(tad, 140, &maxCount, &sensorName, date);
-    
+    assert(maxCount == 61879);
+    assert(strcmp(sensorName,"Street 34") == 0);
+    for (j = 0; j < DATE_FIELDS; j++) {
+        assert(date[j] == date2[j]);
+    }
+    getMaxReadingById(tad, 40, &maxCount, &sensorName, date);
+    assert(maxCount ==  89726);
+    assert(strcmp(sensorName,"Street 1") == 0);
+    for (j = 0; j < DATE_FIELDS; j++) {
+        assert(date[j] == date5[j]);
+    }
+
 }
 
